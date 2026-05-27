92e381820df94a96b31c92b8e7a5b50b2c032b8f
Tunglies <77394545+Tunglies@users.noreply.github.com>
2026-05-27 14:24:08 +0800

    Minimal UDP server DNS forward

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,43 @@ use tokio::net::UdpSocket;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //! Cannot bind to port 5353 on macOS yet
     // let addr: SocketAddr = "0.0.0.0:5353".parse()?;
-    let addr: SocketAddr = "0.0.0.0:1024".parse()?;
-    let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    let server_addr: SocketAddr = "0.0.0.0:1024".parse()?;
+    let client_addr: SocketAddr = "0.0.0.0:0".parse()?;
+    let upstream_dns: SocketAddr = "8.8.8.8:53".parse()?;
 
-    socket.set_reuse_address(true)?;
-    socket.set_reuse_port(true)?;
-    socket.set_nonblocking(true)?;
-    socket.bind(&addr.into())?;
+    let server_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
+    let upstream_socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
 
-    let std_socket: std::net::UdpSocket = socket.into();
+    server_socket.set_reuse_address(true)?;
+    server_socket.set_reuse_port(true)?;
+    server_socket.set_nonblocking(true)?;
+    server_socket.bind(&server_addr.into())?;
+    upstream_socket.set_nonblocking(true)?;
+    upstream_socket.bind(&client_addr.into())?;
 
-    let tokio_socket = UdpSocket::from_std(std_socket)?;
-    println!("UDP server listening on {}", addr);
+    let server_std_socket: std::net::UdpSocket = server_socket.into();
+    let upstream_std_socket: std::net::UdpSocket = upstream_socket.into();
+    let server = UdpSocket::from_std(server_std_socket)?;
+    let upstream = UdpSocket::from_std(upstream_std_socket)?;
+
+    println!("UDP server listening on {}", server_addr);
 
     let mut buf = [0u8; 1024];
     loop {
         println!("Waiting for incoming UDP packets...");
-        let (len, src) = tokio_socket.recv_from(&mut buf).await?;
+        let (len, src) = server.recv_from(&mut buf).await?;
         println!("Received {} bytes from {}", len, src);
-        tokio_socket.send_to(&buf[..len], src).await?;
-        println!("Sent {} bytes back to {}", len, src);
-        // Here you can process the received data in `buf[..len]`
+
+        upstream.send_to(&buf[..len], upstream_dns).await?;
+        println!(
+            "Forwarded packet to upstream DNS server at {}",
+            upstream_dns
+        );
+
+        let (len, _) = upstream.recv_from(&mut buf).await?;
+        println!("Received response from upstream DNS server");
+
+        server.send_to(&buf[..len], src).await?;
+        println!("Sent response back to client at {}", src);
     }
 }
