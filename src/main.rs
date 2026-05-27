fe7c1ae3129a8ad1b82cf961c95b84b5990a2014
Tunglies <77394545+Tunglies@users.noreply.github.com>
2026-05-27 15:28:21 +0800

    Fix mistake share bytes poll across server and client

--- src/main.rs
+++ src/main.rs
@@ -32,30 +32,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("UDP server listening on {}", server_addr);
 
-    let mut buf = [0u8; 1024];
     loop {
+        let mut request_buf = [0u8; 1024];
         println!("Waiting for incoming UDP packets...");
-        let (len, src) = server.recv_from(&mut buf).await?;
+        let (len, src) = server.recv_from(&mut request_buf).await?;
         println!("Received {} bytes from {}", len, src);
-        let dns_parse = parse_dns_header(&buf[..len]).unwrap();
+        let dns_parse = parse_dns_header(&request_buf[..len]).unwrap();
         println!("Parsed DNS header: {:?}", dns_parse);
-        let dns_question_parse = parse_dns_question(&buf[..len], 12).unwrap();
+        let dns_question_parse = parse_dns_question(&request_buf[..len], 12).unwrap();
         println!("Parsed DNS question: {}", dns_question_parse.0);
 
-        upstream.send_to(&buf[..len], upstream_dns).await?;
+        upstream.send_to(&request_buf[..len], upstream_dns).await?;
         println!(
             "Forwarded packet to upstream DNS server at {}",
             upstream_dns
         );
 
-        let (len, _) = upstream.recv_from(&mut buf).await?;
+        let mut response_buf = [0u8; 1024];
+        let (len, _) = upstream.recv_from(&mut response_buf).await?;
         println!("Received response from upstream DNS server");
-        let dns_response_parse = parse_dns_header(&buf[..len]).unwrap();
+        let dns_response_parse = parse_dns_header(&response_buf[..len]).unwrap();
         println!("Parsed DNS response header: {:?}", dns_response_parse);
 
         let mut offset = 12;
         for _ in 0..dns_response_parse.qdcount {
-            let (question_parse, next_offset) = parse_dns_question(&buf[..len], offset).unwrap();
+            let (question_parse, next_offset) =
+                parse_dns_question(&response_buf[..len], offset).unwrap();
             println!(
                 "Parsed DNS question(total: {}) in response: {}",
                 dns_response_parse.qdcount, question_parse
@@ -63,7 +65,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             offset = next_offset;
         }
         for _ in 0..dns_response_parse.ancount {
-            let (anser_parse, next_offset) = parse_dns_answer(&buf[..len], offset).unwrap();
+            let (anser_parse, next_offset) =
+                parse_dns_answer(&response_buf[..len], offset).unwrap();
             println!(
                 "Parsed DNS answer(total: {}) in response: {}",
                 dns_response_parse.ancount, anser_parse
@@ -71,7 +74,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             offset = next_offset;
         }
 
-        server.send_to(&buf[..len], src).await?;
+        server.send_to(&response_buf[..len], src).await?;
         println!("Sent response back to client at {}", src);
     }
 }
